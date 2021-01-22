88b05b748320d0d27a1b6f90a8bd883c03998f19
sandip <sandip@588>
2021-01-22 15:26:36 +0530

    initial commit

---
+++ app/src/main/java/com/mediasupportingapplication/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.mediasupportingapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+     var media:String=""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+
+        btnonClickMp4.setOnClickListener {
+            var intent = Intent(this, VidepPlayerActivity::class.java)
+            media = " https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
+            intent.putExtra("media",media)
+            intent.putExtra("mediaType","mp4")
+            startActivity(intent)
+        }
+        btnonClickAVI.setOnClickListener {
+            var intent = Intent(this, VidepPlayerActivity::class.java)
+            media = " https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-avi-file.avi"
+            intent.putExtra("mediaType","avi")
+            intent.putExtra("media",media)
+            startActivity(intent)
+        }
+        btnonClickMOV.setOnClickListener {
+            var intent = Intent(this, VidepPlayerActivity::class.java)
+            media = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mov-file.mov"
+            intent.putExtra("media",media)
+            intent.putExtra("mediaType","mov")
+            startActivity(intent)
+        }
+        btnonClickMPG.setOnClickListener {
+            var intent = Intent(this, VidepPlayerActivity::class.java)
+            media = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mpg-file.mpg"
+            intent.putExtra("mediaType","mpg")
+            intent.putExtra("media",media)
+            startActivity(intent)
+        }
+
+        btnonClickWMV.setOnClickListener {
+            var intent = Intent(this, VidepPlayerActivity::class.java)
+            media = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-wmv-file.wmv"
+            intent.putExtra("mediaType","wmv")
+            intent.putExtra("media",media)
+            startActivity(intent)
+        }
+        btnonClickFLV.setOnClickListener {
+            var intent = Intent(this, VidepPlayerActivity::class.java)
+            media = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-flv-file.flv"
+            intent.putExtra("mediaType","flv")
+            intent.putExtra("media",media)
+            startActivity(intent)
+        }
+        btnonClickM4A.setOnClickListener {
+            var intent = Intent(this, VidepPlayerActivity::class.java)
+            media = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mov-file.mov"
+            intent.putExtra("mediaType","mov")
+            intent.putExtra("media",media)
+            startActivity(intent)
+        }
+        btnonClickWEBM.setOnClickListener {
+            var intent = Intent(this, VidepPlayerActivity::class.java)
+            media = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-webm-file.webm"
+            intent.putExtra("mediaType","webm")
+            intent.putExtra("media",media)
+            startActivity(intent)
+        }
+
+        btnonClickMKV.setOnClickListener {
+            var intent = Intent(this, VidepPlayerActivity::class.java)
+            media = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mkv-file.mkv"
+            intent.putExtra("mediaType","mkv")
+            intent.putExtra("media",media)
+            startActivity(intent)
+        }
+        btnonClickOGV.setOnClickListener {
+            var intent = Intent(this, VidepPlayerActivity::class.java)
+            media = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-ogv-file.ogv"
+            intent.putExtra("mediaType","ogv")
+            intent.putExtra("media",media)
+            startActivity(intent)
+        }
+        btnonClick3GP.setOnClickListener {
+            var intent = Intent(this, VidepPlayerActivity::class.java)
+            media = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-3gp-file.3gp"
+            intent.putExtra("mediaType","3gp")
+            intent.putExtra("media",media)
+            startActivity(intent)
+        }
+        btnonClick3G2.setOnClickListener {
+            var intent = Intent(this, VidepPlayerActivity::class.java)
+            media = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-3g2-file.3g2"
+            intent.putExtra("mediaType","3gp")
+            intent.putExtra("media",media)
+            startActivity(intent)
+        }
+
+
+    }
+
+}
---
+++ app/src/main/java/com/mediasupportingapplication/VidepPlayerActivity.kt
@@ -0,0 +1,38 @@
+package com.mediasupportingapplication
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import kotlinx.android.synthetic.main.activity_videoplayer.*
+import java.util.*
+
+
+class VidepPlayerActivity: AppCompatActivity() {
+    var media:String=""
+    var mediaType:String=""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_videoplayer)
+        initView()
+    }
+
+    private fun initView() {
+        media=intent.getStringExtra("media")!!
+       textMediaType.text="Media Type:"+intent.getStringExtra("mediaType")!!
+       val player = SimpleExoPlayer.Builder(this).build()
+       PlayerViewSData.setPlayer(player)
+        // Build the media items.
+        val rnd = Random()
+        val color: Int = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+        clBackground.setBackgroundColor(color)
+   val firstItem: MediaItem = MediaItem.fromUri(media)
+        player.apply {
+            addMediaItem(firstItem)
+            prepare()
+            play()
+
+        }
+    }
+}
